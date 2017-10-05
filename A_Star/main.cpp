f0695d44880e54d14e8a8d8e7ffebf2c4c94dacb
pawmastalerz <paw.mastalerz@gmail.com>
2017-10-05 17:29:05 +0200

    Show zeroed nodes array.

--- A_Star/main.cpp
+++ A_Star/main.cpp
@@ -1,9 +1,18 @@
 #include <iostream>
+#include <fstream>
 
 using namespace std;
 
 int main()
 {
-    cout << "Hello world!" << endl;
+    int nodes[20][20] = {0};
+    for(int i = 0; i < 20; i++)
+    {
+        for(int j = 0; j < 20; j++)
+        {
+            cout << nodes[i][j] << " ";
+        }
+        cout << endl;
+    }
     return 0;
 }
