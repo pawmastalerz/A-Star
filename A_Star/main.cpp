e08c14407ada38af06b23e6be2159e5ab82333a8
pawmastalerz <paw.mastalerz@gmail.com>
2017-10-06 21:42:40 +0200

    Euklidean bugfix.

--- A_Star/main.cpp
+++ A_Star/main.cpp
@@ -15,21 +15,21 @@ bool openList[20][20];
 bool closedList[20][20];
 int currentX = 0;
 int currentY = 0;
-int parent[20][20];
+string parent[20][20];
 
-void readNodeValuesFromFile()
+void readNodeValuesFromFile() // EUKLIDEAN CHECK
 {
     FILE *inputGrid;
     inputGrid = fopen("C:\\Users\\Home\\Desktop\\A gwiazdka\\Pawel_Mastalerz\\AStar\\A_Star\\bin\\Debug\\grid.txt", "r");
     if (inputGrid != NULL)
     {
         char gridNumericValue[3]; // No idea why there should be 3 instead of 2.
-        for (int i = 0; i < 20; i++)
+        for (int i = 19; i >= 0; i--)
         {
             for(int j = 0; j < 20; j++)
             {
                 fgets(gridNumericValue, 3, inputGrid); // Same here. It works, so not that I'm complaining.
-                nodeValues[i][j] = (gridNumericValue[0]);
+                nodeValues[j][i] = (gridNumericValue[0]);
             }
         }
     }
@@ -37,38 +37,14 @@ void readNodeValuesFromFile()
     fclose(inputGrid);
 }
 
-void showNodeValues()
-{
-    for(int i = 0; i < 20; i++)
-    {
-        for(int j = 0; j < 20; j++)
-        {
-            cout << nodeValues[i][j] << " ";
-        }
-        cout << endl;
-    }
-}
-
 void calculateHValues()
 {
     for (int i = 0; i < 20; i++)
     {
-        for(int j = 0; j < 20; j++)
+        for (int j = 0; j < 20; j++)
         {
             hValues[i][j] = i + j;
         }
-    }
-}
-
-void showHValues()
-{
-    for(int i = 0; i < 20; i++)
-    {
-        for(int j = 0; j < 20; j++)
-        {
-            cout << hValues[i][j] << " ";
-        }
-        cout << endl;
     }
 }
 
@@ -94,7 +70,7 @@ void startListsAndArrays()
     {
         for(int j = 0; j < 20; j++)
         {
-            parent[i][j] = 99;
+            parent[i][j] = "undef";
         }
     }
 }
@@ -104,6 +80,15 @@ void closeCurrent()
     closedList[currentX][currentY] = true;
 }
 
+//int goUp()
+//{
+//    if ((currentY + 1) > 19) || closedList[currentX][currentY - 1] == true) return 0;
+//    else
+//    {
+//        parent[currentX][current]
+//    }
+//}
+
 int main()
 {
     readNodeValuesFromFile();
@@ -112,14 +97,16 @@ int main()
 
     closeCurrent();
 
-    for(int i = 0; i < 20; i++)
+    for (int i = 19; i >= 0; i--)
     {
         for(int j = 0; j < 20; j++)
         {
-            cout << closedList[i][j] << " ";
+            cout << hValues[j][i] << " ";
         }
         cout << endl;
     }
+//    cout << endl << endl;
+//    cout << nodeValues[11][0];
 
     return 0;
 }
