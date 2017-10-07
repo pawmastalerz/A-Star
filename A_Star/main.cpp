850fd21ec7843b21f86c67c606d9cb8e6dba5115
pawmastalerz <paw.mastalerz@gmail.com>
2017-10-07 15:19:33 +0200

    Calculate lowest F (x,y) position.

--- A_Star/main.cpp
+++ A_Star/main.cpp
@@ -8,17 +8,17 @@
 
 using namespace std;
 
-
 char nodeValues[20][20];
 int hValues[20][20];
 int gValues[20][20];
 int fValues[20][20];
-bool openList[20][20];
+bool openedList[20][20];
 bool closedList[20][20];
 int currentX = 0;
 int currentY = 0;
 int parentX[20][20];
 int parentY[20][20];
+
 
 void readNodeValuesFromFile()
 {
@@ -58,7 +58,7 @@ void startListsAndArrays()
         for(int j = 0; j < 20; j++)
         {
             closedList[i][j] = false;
-            openList[i][j] = false;
+            openedList[i][j] = false;
             parentX[i][j] = 999;
             parentY[i][j] = 999;
             gValues[i][j] = 999;
@@ -70,9 +70,14 @@ void startListsAndArrays()
     gValues[0][0] = 0;
 }
 
-void closeCurrent()
+void closeNode(int x, int y)
 {
-    closedList[currentX][currentY] = true;
+    closedList[x][y] = true;
+}
+
+void openNode(int x, int y)
+{
+    openedList[x][y] = true;
 }
 
 void calculateGValue(int x, int y)
@@ -90,6 +95,7 @@ int goUp()
     if (((currentY + 1) > 19) || (closedList[currentX][currentY + 1] == true)) return 0;
     else
     {
+        openNode(currentX, currentY + 1);
         parentX[currentX][currentY + 1] = currentX;
         parentY[currentX][currentY + 1] = currentY;
         calculateGValue(currentX, currentY + 1);
@@ -102,6 +108,7 @@ int goDown()
     if (((currentY - 1) < 0) || (closedList[currentX][currentY - 1] == true)) return 0;
     else
     {
+        openNode(currentX, currentY - 1);
         parentX[currentX][currentY - 1] = currentX;
         parentY[currentX][currentY - 1] = currentY;
         calculateGValue(currentX, currentY - 1);
@@ -114,6 +121,7 @@ int goLeft()
     if (((currentX - 1) < 0) || (closedList[currentX - 1][currentY] == true)) return 0;
     else
     {
+        openNode(currentX - 1, currentY);
         parentX[currentX - 1][currentY] = currentX;
         parentY[currentX - 1][currentY] = currentY;
         calculateGValue(currentX - 1, currentY);
@@ -126,6 +134,7 @@ int goRight()
     if (((currentX + 1) > 19) || (closedList[currentX + 1][currentY] == true)) return 0;
     else
     {
+        openNode(currentX + 1, currentY);
         parentX[currentX + 1][currentY] = currentX;
         parentY[currentX + 1][currentY] = currentY;
         calculateGValue(currentX + 1, currentY);
@@ -133,12 +142,48 @@ int goRight()
     }
 }
 
+int calculateLowestFXPos()
+{
+    int lowest = 9999;
+    int lowestXPos = 9999;
+    for (int i = 0; i < 19; i++)
+    {
+        for (int j = 0; j < 19; j++)
+        {
+            if ((openedList[i][j] == true) && (fValues[i][j] < lowest))
+            {
+                lowest = fValues[i][j];
+                lowestXPos = i;
+            }
+        }
+    }
+    return lowestXPos;
+}
+
+int calculateLowestFYPos()
+{
+    int lowest = 9999;
+    int lowestYPos = 9999;
+    for (int i = 0; i < 19; i++)
+    {
+        for (int j = 0; j < 19; j++)
+        {
+            if ((openedList[i][j] == true) && (fValues[i][j] < lowest))
+            {
+                lowest = fValues[i][j];
+                lowestYPos = j;
+            }
+        }
+    }
+    return lowestYPos;
+}
+
 int main()
 {
     readNodeValuesFromFile();
     startListsAndArrays();
 
-    closeCurrent();
+    closeNode(currentX, currentY);
     goUp();
     goDown();
     goLeft();
@@ -148,10 +193,13 @@ int main()
     {
         for(int j = 0; j < 20; j++)
         {
-            cout << hValues[j][i] << " ";
+            cout << fValues[j][i] << " ";
         }
         cout << endl;
     }
+
+    cout << calculateLowestFXPos() << endl;
+    cout << calculateLowestFYPos() << endl;
 
     return 0;
 }
